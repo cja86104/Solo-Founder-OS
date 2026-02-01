180f9946402d4d1812c42c89373762f0d6359ef3
cja86104 <cja86104@gmail.com>
2026-02-01 08:29:12 -0500

    LANDING UPDATE

--- app/page.tsx
+++ app/page.tsx
@@ -228,7 +228,7 @@ export default function HomePage() {
             </p>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6">
             {products.map((product) => (
               <div
                 key={product.name}
